43c773a7ccdcd9ca34388ccd30c75dd94c1dd842
Dazai-red <joseanlopezavila@gmail.com>
2022-11-04 20:38:40 +0100

    dar formato con prettier

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
-const grande = document.querySelector('.grande');
-const punto = document.querySelectorAll('.punto');
+const grande = document.querySelector(".grande");
+const punto = document.querySelectorAll(".punto");
 
 console.log(grande);
 console.log(punto);
 
 // Cuando hago click en cada punto
-    // saber la posición de ese punto
-    // Aplicar un transform translateX al grande
-    // QUITAR la clase activo de todos los puntos
-    // Añadir la clase activo al punto que hemos hecho click
+// saber la posición de ese punto
+// Aplicar un transform translateX al grande
+// QUITAR la clase activo de todos los puntos
+// Añadir la clase activo al punto que hemos hecho click
 
-punto.forEach( (cadaPunto, i )=>{
-    punto[i].addEventListener('click',()=>{
+punto.forEach((cadaPunto, i) => {
+  punto[i].addEventListener("click", () => {
+    // 50% porque es el ancho de las dos img
+    // posicion = 0 transformX es 0
+    // posicion = 1 transformX es -50%
+    // operacion = operacion * -50
+    let posicion = i;
+    let operacion = posicion * -50;
 
-        // 50% porque es el ancho de las dos img
-        // posicion = 0 transformX es 0
-        // posicion = 1 transformX es -50%
-        // operacion = operacion * -50
-        let posicion = i;
-        let operacion = posicion * -50;
+    grande.style.transform = `translateX(${operacion}%)`;
+    //console.log(operacion);
 
-        grande.style.transform = `translateX(${operacion}%)`;
-        //console.log(operacion);
+    punto.forEach((cadaPunto, i) => {
+      punto[i].classList.remove("activo");
+    });
 
-        punto.forEach( (cadaPunto, i)=>{
-            punto[i].classList.remove('activo');
-        })
-        
-        punto[i].classList.add('activo');
-
-    })
-})
+    punto[i].classList.add("activo");
+  });
+});
